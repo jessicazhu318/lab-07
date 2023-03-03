551098071165ffe57cbd243a010d7c5535a4884c
jessicazhu <jessicazhu@Ubuntu22.04.myguest.virtualbox.org>
2023-03-02 16:40:42 -0800

    Initial commit

---
+++ test_suite.py
@@ -0,0 +1,15 @@
+# EE 250: Lab 7
+
+if __name__ == '__main__':
+
+   # Blink LED 5x w/ on/off intervals of 500ms
+   
+   # For ~5sec, read output of Grove light sensor w/ intervals of 100ms
+   #and print raw value along w/ text "bright" or "dark" (determine threshold
+   #through experimentation)
+   
+   # Blink LED 4x w/ on/off intervals of 200ms
+   
+   # For ~5sec, read output of Grove sound sensor w/ intervals of 100ms
+   #and print raw value, if sensor is tapped (ie. sound magnitude exceeds
+   #threshold decided from experiment), LED should turn on for 100ms
